7e35e5f4cb2d8dada69e2e61fcabd32d278eda4b
наталя ДУЖАК <Duzhak.N@gmail.com>
2022-11-18 13:22:09 +0200

    first commit

---
+++ js/menu.js
@@ -0,0 +1,42 @@
+
+/*let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})*/
+/*jQuery(document).ready(function() {
+	jQuery('.toggle-nav').click(function(e) {
+		jQuery(this).toggleClass('active');
+		jQuery('.menu ul').toggleClass('active');
+
+		e.preventDefault();
+	});
+});*/
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu__button');
+    let links = menu.find('.burger-menu__link');
+    let overlay = menu.find('.burger-menu__overlay');
+    
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+    
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+    
+    function toggleMenu() {
+        menu.toggleClass('burger-menu_active');
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
+}
+
+burgerMenu ('.burger-menu');
